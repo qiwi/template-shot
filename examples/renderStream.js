2b68d8649159ed6cc364a901d7ecf96cb9dd8bd8
dmitry <dimdi-y@ya.ru>
2017-08-04 15:44:27 +0300

    added dummy test

--- examples/renderStream.js
+++ examples/renderStream.js
@@ -1,10 +1,12 @@
 /** this example renders the template into a stream
- * and then returns this image sream as a response for http request */
+ * and then returns this image sream as a response for http request
+ * the template value is taken from the query string */
 
 
 const {TemplateShot} =require( '../build/index');
 // Load the http module to create an http server.
 var http = require('http');
+var url = require("url");
 
 const templatePath = __dirname+'/templates';
 console.log('looking for templates in:\n' + templatePath);
@@ -15,18 +17,24 @@ const ts = new TemplateShot(templatePath);
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
+    const parsedUrl = url.parse(request.url, true); // true to get query as object
+    let text = parsedUrl.query.text;
+    if (!text || text == ''){
+        text = 'No text parameter was passed. Add the `?text=your_text_here` to the end of your url';
+    }
+    
     // return the header for image
     response.writeHead(200, {"Content-Type": "image/png"});
 
     console.log('rendering template index.html\
-        with "paragraph_text" parameter value set as "Test text"\
+        with "paragraph_text" parameter value set as ' + text + '\
         with custom screen size\
         to stream and then piping the stream to http response');
 
     // render stream init
     const renderStream = ts.renderStream(
         'index.html',
-        {'paragraph_text' : 'Stream is rendered to http'},
+        {'paragraph_text' : text},
         {   
             screenSize: {
                 width: 540,
---
+++ src/test/tests.ts
@@ -0,0 +1,10 @@
+import {TemplateShot} from '../index';
+import {expect} from 'chai';
+
+describe('test suite', function(): void{
+    // TODO: add tests
+    it('dummy', function(done: MochaDone): void {
+        expect(true).to.be.eql(true);
+        done();
+    });
+});
