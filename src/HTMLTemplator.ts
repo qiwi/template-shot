71ee0471f8c66a54d9ed1341f217c8075e8ec8ec
dmitry <dimdi-y@ya.ru>
2017-08-07 17:03:11 +0300

    added cache

--- src/HTMLTemplator.ts
+++ src/HTMLTemplator.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import * as readFile from 'fs-readfile-promise';
+import {readFile} from 'fs';
 import * as format from 'string-template';
 
 export interface ITemplateValues {
@@ -7,20 +7,41 @@ export interface ITemplateValues {
 }
 
 export class HTMLTemplator {
-    constructor(private templateDir: string = '/') {
+    private templateCache: Map<string, string> = new Map<string, string>();
+    constructor(private templateDir: string = '/', private useCache: boolean = false) {
         // TODO: make this work relatively to the file where instance is created
         if (this.templateDir.startsWith('./')) {
             this.templateDir = path.join(__dirname, templateDir);
         }
     }
-    public async generateHTML(templateName: string, templateValues: ITemplateValues = {}): Promise<string> {
+
+    public async generateHTML(
+        templateName: string,
+        templateValues: ITemplateValues = {}
+    ): Promise<string> {
         // TODO: add the ability to load template from string
         const templatePath: string = path.join(this.templateDir, templateName);
-        const template: string = (await readFile(templatePath)).toString();
+        const template: string = await this.readCachedFile(templatePath);
         // TODO: use cache here
         templateValues.templateDir = 'file://' + this.templateDir;
         // TODO: allow using lodash
         const res = format(template, templateValues);
         return res;
+    }
+
+    private async readCachedFile(path: string): Promise<string> {
+        if (this.useCache && this.templateCache.has(path)) {
+            return Promise.resolve(this.templateCache.get(path));
+        } else {
+            return new Promise<string>((resolve: any, reject: any): void => {
+                readFile(path, (err: Error, template: Buffer) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(template.toString);
+                    }
+                });
+            });
+        }
     }
 }
