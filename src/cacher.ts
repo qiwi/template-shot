7f830e508d50e0428f51e895892f9fdb40e68a0f
dmitry <dimdi-y@ya.ru>
2017-08-08 17:16:43 +0300

    FIXED on the pr commented changes

--- src/cacher.ts
+++ src/cacher.ts
@@ -1,8 +1,8 @@
 import {EventEmitter} from 'events';
 
 export class Cacher<T> extends EventEmitter {
-    public static readonly TIMEOUT_ERROR_MESSAGE: string = 'Cahcer.get timed out,\
-because the value was not provided by Cacher.set';
+    public static readonly TIMEOUT_ERROR_MESSAGE: string = 'Cacher.get timed out\
+because the value was not provided';
     private cache: Map<string, T | Promise<T> > = new Map<string, T | Promise<T> >(); // TODO; add smarter cache here
     constructor(private noResponseTimeout: number = -1) {super(); }
 
@@ -13,9 +13,13 @@ because the value was not provided by Cacher.set';
     public async get(key: string): Promise<T> {
         if (this.cache.has(key)) {
             const res: any = this.cache.get(key);
+            let to;
             if (typeof(res.then) === 'function') { // if true, we have a Promise<T>
-                return this.noResponseTimeout > 0 ? Promise.race([res, new Promise((resolve, reject) => {
-                    setTimeout(() => reject(new Error(Cacher.TIMEOUT_ERROR_MESSAGE)), this.noResponseTimeout);
+                return this.noResponseTimeout > 0 ? Promise.race([new Promise((resolve, reject) => {
+                    to = setTimeout(() => reject(new Error(Cacher.TIMEOUT_ERROR_MESSAGE)), this.noResponseTimeout);
+                }), res.then((val) => {
+                    clearTimeout(to);
+                    return val;
                 })]) : res;
             } else {// else we have T
                 return Promise.resolve(res);
